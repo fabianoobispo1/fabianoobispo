740edb44a7ac7e3e37e13927f849539709a2417a
fabianoobispo1 <fabianoobispo@icloud.com>
2022-04-20 23:51:18 -0300

    corecao

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 import packageInfo  from '../../package.json'
 
 export const stripe = new Stripe(
-    process.env.STRIPE_API_KEY,
+    process.env.STRIPE_API_KEY!,
     {
         apiVersion: '2020-08-27',
         appInfo: {
