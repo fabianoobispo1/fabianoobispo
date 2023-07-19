55c47f868321cd664a6c15044a051228cd6022da
fabianoobispo1 <fabianoobispo@icloud.com>
2023-07-19 20:48:58 -0300

    teste de layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Fabiano Bispo</h1>
+import { db } from '@/db'
+import { users } from '@/db/schema'
+
+export default async function Home() {
+  const allUsers = await db.select().from(users)
+  console.log(allUsers)
+  return <p className="text-cyan-300">{JSON.stringify(allUsers, null, 2)}</p>
 }
