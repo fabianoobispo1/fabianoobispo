c6252070e43734cd4a44a270ffb70d8a87e01e94
fabianoobispo1 <fabianoobispo@icloud.com>
2023-10-19 11:33:44 -0300

    alteracao no menu 1

--- src/app/api/minhaapi/route.ts
+++ src/app/api/minhaapi/route.ts
@@ -1,6 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+type ResponseData = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
   const data = { message: 'Hello from the API!' }
   res.status(200).json(data)
 }
