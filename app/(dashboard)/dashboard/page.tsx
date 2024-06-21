0674685b276407e85f4eab8686a60e00e54aec48
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-06-21 19:53:58 +0000

    tela inicial

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -14,9 +14,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function page() {
   return (
-    {/* <ScrollArea className="h-full">
+    <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
-        <div className="flex items-center justify-between space-y-2">
+      {/*   <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
             Hi, Welcome back 👋
           </h2>
@@ -158,8 +158,8 @@ export default function page() {
               </Card>
             </div>
           </TabsContent>
-        </Tabs>
+        </Tabs>*/}
       </div>
-    </ScrollArea> */}
+    </ScrollArea> 
   );
 }
