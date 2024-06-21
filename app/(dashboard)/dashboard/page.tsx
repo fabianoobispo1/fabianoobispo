120dc57a779579acc5c66aabaeeee2532e4cb985
Fabiano Bispo <95259654+fabianoobispo1@users.noreply.github.com>
2024-06-21 19:31:37 +0000

    tela plano hmg

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -14,7 +14,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 export default function page() {
   return (
-    <ScrollArea className="h-full">
+    {/* <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">
@@ -160,6 +160,6 @@ export default function page() {
           </TabsContent>
         </Tabs>
       </div>
-    </ScrollArea>
+    </ScrollArea> */}
   );
 }
