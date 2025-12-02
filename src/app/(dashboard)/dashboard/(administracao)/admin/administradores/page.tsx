d032ed036a17c40a0ecf2c4e6e300ef26d36e264
fabianoobispo1 <fabianoobispo@icloud.com>
2025-12-02 08:45:03 -0300

    testess

--- src/app/(dashboard)/dashboard/(administracao)/admin/administradores/page.tsx
+++ src/app/(dashboard)/dashboard/(administracao)/admin/administradores/page.tsx
@@ -15,7 +15,7 @@ export default function page() {
         <div className=" flex items-start justify-between gap-4">
           <Heading
             title={'Administradores'}
-            description={'Alterar usuario para adminsitrador'}
+            description={'Alterar usuario para adminsitrador.'}
           />
         </div>
         <UsuariosAdministradores />
